607a6bb63b2929948946f688b2c1644247851313
yuka-mm <k1105my@ymail.ne.jp>
2024-01-10 02:04:09 +0900

    add ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'top#top'
+  # ログイン機能
+  devise_for :parents
+  devise_scope :parent do
+    get '/parents/sign_out' => 'devise/sessions#destroy'
+  end
 
+  # トップページ
+  root to: 'top#top'
   get 'top_page', to: 'top#top'
 end
